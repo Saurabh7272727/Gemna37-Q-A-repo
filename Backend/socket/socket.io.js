c12bf0218fc8f05d3adfd43083959e34760a8457
saurabh sharma <saurabhsharma63679383@gmail.com>
2025-10-25 22:33:33 +0530

    fix issues like add transports array on socket.io

--- Backend/socket/socket.io.js
+++ Backend/socket/socket.io.js
@@ -8,10 +8,14 @@ const connectWithSocket = (server) => {
     try {
         const io = new Server(server, {
             cors: {
-                origin: ["http://localhost:5173", "https://gemna37-q-a-repo.vercel.app"],
-                methods: ["POST", "GET"],
+                origin: [
+                    "http://localhost:5173", process.env.FRONTEND_URL
+                ],
+                methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
                 credentials: true,
-            }
+                allowedHeaders: ["Content-Type", "Authorization"]
+            },
+            transports: ['websocket', 'polling']
         });
 
         io.use(async (socket, next) => {
