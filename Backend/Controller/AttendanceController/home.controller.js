928ea9b76ee0cb49d2f1d4324d0532d3ff08d6fe
saurabh sharma <saurabhsharma63679383@gmail.com>
2026-01-26 14:49:32 +0530

    Our team add redis-stack server t0 cached the sessions and security purpose to hold secure transaction

--- Backend/Controller/AttendanceController/home.controller.js
+++ Backend/Controller/AttendanceController/home.controller.js
@@ -1,21 +1,23 @@
 import * as z from "zod";
 import StudentAttendanceModel from "../../model/Attendence_Section/StudentAttendenceModel.js";
-import jwt from "jsonwebtoken";
-import { encryptData } from "../../components/crypto.js";
+import { encryptData, decryptData } from "../../components/crypto.js";
 
-const mapHoldRecordRunning = new Map();
+// redis import
+import redis from "../../service/Redis/Client.js";
+
 
 const objPayloadVerification = z.object({
     gemidlog: z.string().length(24).regex(/^[a-f\d]{24}$/i),
-    timestamp: z.number()
+    timestamp: z.string()
 });
 
 const homeController = async (req, res) => {
-    const { gemidlog } = req.body;
-
+    const { gemidlog, timestamp, sessionToken } = req.body;
     try {
-        const parsed = objPayloadVerification.safeParse(req.body);
 
+        // Validation of Payload => body part
+
+        const parsed = objPayloadVerification.safeParse({ gemidlog, timestamp });
         if (!parsed.success) {
             return res.status(404).json({
                 status: 404,
@@ -23,105 +25,134 @@ const homeController = async (req, res) => {
                 success: false
             });
         }
-
         req.body = parsed.data;
-        const findUserByGemidlog = await StudentAttendanceModel.findOne({ gemidlog });
 
-        /* ========================================================
-           CASE 1: USER NOT FOUND + SESSION ALREADY RUNNING
-        ======================================================== */
-        if (!findUserByGemidlog && mapHoldRecordRunning.has(gemidlog)) {
-            const valueAttend = mapHoldRecordRunning.get(gemidlog);
+        const findUserInAttendanceRecord = await StudentAttendanceModel.findOne({ gemidlog });
 
-            if (valueAttend.attend > 5) {
-                return res.status(429).json({
-                    status: 429,
-                    message: `${valueAttend.attend} : you limit the process count`,
-                    success: false
-                });
-            }
-
-            mapHoldRecordRunning.set(gemidlog, {
-                gemidlog,
-                attend: valueAttend.attend + 1
+        if (findUserInAttendanceRecord) {
+            res.status(200).json({
+                message: "User found in attendance record",
+                status: 200,
+                success: false,
+                sessionToken: encryptData({
+                    gemidlog,
+                    expiration: Date.now(),
+                    permission: {
+                        role: "Student",
+                        update: true,
+                        upload: true,
+                        admin: false,
+                        routePermission: true
+                    }
+                })
             });
-
-            return res.status(409).json({
-                status: 409,
-                message: "You already opened the session window in another tab or browser",
-                success: false
-            });
+            return;
         }
 
+        // check user are exist or not in redis session group
+        const redisResponseSessionExist = await redis.hgetall(`session:${gemidlog}`);
 
-        if (!findUserByGemidlog && !mapHoldRecordRunning.has(gemidlog)) {
-            mapHoldRecordRunning.set(gemidlog, {
-                gemidlog,
-                attend: 1
-            });
-
-            const sessionToken = {
-                gemidlog,
-                timestamp: Date.now(),
-                expiresIn: '10m'
-            }
-
-            return res.status(202).json({
-                status: 202,
-                message: "User not found in record",
-                success: true,
-                sessionToken: encryptData(sessionToken)
-            });
-        }
-
-        if (findUserByGemidlog?.gemidlog) {
-            const sessionToken = jwt.sign(
-                { gemidlog, timestamp: Date.now() },
-                process.env.JSON_SECRET_KEY,
-                { expiresIn: "24h" }
+        // if not exist to allowed for session
+        if (Object.keys(redisResponseSessionExist).length === 0 && !sessionToken) {
+            const hashSetInRedis = await redis.hset(
+                `session:${gemidlog}`,
+                {
+                    'gemidlog': `${gemidlog}`,
+                    'attend': 1,
+                    'expiration': `${Date.now()}`,
+                }
             );
 
-            return res.status(200).json({
-                status: 200,
-                message: "Verification successfully completed",
-                success: true,
-                sessionToken: encryptData(sessionToken)
-            });
+            // set expiration 300seconds = 5min
+            await redis.expire(`session:${gemidlog}`, 300);
+
+            if (hashSetInRedis === 3) {
+                const getValueFromRedis = await redis.hgetall(`session:${gemidlog}`);
+                res.status(202).json({
+                    message: "User not found but session allowed",
+                    status: 202,
+                    success: false,
+                    sessionToken: encryptData(getValueFromRedis)
+                });
+                return;
+            } else {
+                throw new Error(`${hashSetInRedis} : will not equal to 3`)
+            }
+
+        } else {
+            // there validate if user have session id and range of redis expiration
+            if (sessionToken) {
+                // decrypt the sessionToken
+                let decryptDataSessionToken;
+                try {
+                    decryptDataSessionToken = decryptData(sessionToken);
+                } catch {
+                    return res.status(401).json({
+                        message: "Invalid session token",
+                        success: false,
+                        status: 401
+                    });
+                }
+
+                // if not present gemidlog field and expiration time
+                if (!decryptDataSessionToken.gemidlog && !decryptDataSessionToken?.expiration) {
+                    throw new Error("session token are invaild format , this not acceptable format..")
+                }
+
+                // if present the check in redis to present or not same session group with gemidlog
+                const { gemidlog } = decryptDataSessionToken
+                let redisResponseSessionExist = await redis.hgetall(`session:${gemidlog}`);
+
+                if (Object.keys(redisResponseSessionExist).length === 0) {
+                    throw new Error("Session will be expired, try again")
+                }
+
+                const incrementTheAttend = await redis.hincrby(`session:${gemidlog}`, 'attend', 1);
+                redisResponseSessionExist = await redis.hgetall(`session:${gemidlog}`);
+
+                // check time exseed
+                if (redisResponseSessionExist?.attend < 5 && (Date.now() - Number(redisResponseSessionExist?.expiration) < 5 * 60 * 1000) && incrementTheAttend < 5) {
+                    res.status(202).json({
+                        message: "User session are rotated with allocate a new session",
+                        status: 202,
+                        success: false,
+                        sessionToken: encryptData(redisResponseSessionExist)
+                    });
+                    return;
+                } else {
+                    res.status(429).json({
+                        message: "Too many request for session , you try again in after 5 mins",
+                        status: 429,
+                        success: false,
+                    });
+                    return;
+                }
+
+            }
         }
 
-
-        throw new Error("Internal server error - homeController");
+        if (redisResponseSessionExist.gemidlog && redisResponseSessionExist?.expiration) {
+            res.status(409).json({
+                message: "Session will be send already, duplication are not allowed",
+                status: 409,
+                success: false,
+            });
+            return;
+        }
 
     } catch (error) {
-        console.error("ERROR ======>", error);
-
-        if (gemidlog) {
-            mapHoldRecordRunning.delete(gemidlog);
-        }
-
-        return res.status(500).json({
-            status: 505,
-            message: "Internal server error",
+        res.status(500).json({
+            message: error.message,
+            status: 500,
             success: false
         });
+        return;
     }
+
 };
 
 const unmountController = async (req, res) => {
-    try {
-        const data = req.body;
-        const { _id } = data;
-        if (_id) {
-            if (mapHoldRecordRunning.has(`${_id}`)) {
-                mapHoldRecordRunning.delete(`${_id}`);
-            }
-        }
-        return res.json({ message: "Cleaning done ...", id: _id, data: [...mapHoldRecordRunning] });
-    } catch (error) {
-        console.log(error)
-        return res.send(error);
-    }
-
+    res.end();
 }
 
 export default { homeController, unmountController };
