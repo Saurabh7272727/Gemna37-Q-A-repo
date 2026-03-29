805748a82f87aad90d3d248f4127a74864c3aa5f
saurabh sharma <saurabhsharma63679383@gmail.com>
2026-03-29 20:34:07 +0530

    PR Documentation — SocketSingleton + Redis Online User System, Our goal  in gemna to implement  multi-device messaging system

--- Backend/socket/socket.io.js
+++ Backend/socket/socket.io.js
@@ -3,10 +3,7 @@ import AuthBYSocket from './AuthBYSocket.js';
 import connectionModel from '../model/connection.model.js';
 import messagemodel from '../model/message.model.js';
 import { acceptOfflineMessages } from '../service/NotificationSystem/offlineFeature.js';
-
-
-const tempStorage = new Map();
-
+import { SocketSingleton } from '../module/socket.signleton.js'
 
 
 const connectWithSocket = (server) => {
@@ -46,27 +43,34 @@ const connectWithSocket = (server) => {
         });
 
         // chating module ........
+        const socketSingleton = new SocketSingleton(io);
         io.on("connection", async (socket) => {
-            console.log(`user info ${socket.id}`);
-            tempStorage.set(socket.id, socket.user);
+            // using singleton method - addUser(user details, socket id)
+            if (socket.id && socket.user) {
+                const permission_to_add = await socketSingleton.addUser(socket.user, socket.id);
+                if (permission_to_add) {
+                    await socket.join(`${socket?.user?.ref_id?.branch?.value}/${socket?.user?.ref_id?.year?.value}`)
+                    const localdata = await socketSingleton.getAllOnlineUser(`${socket.user?.ref_id.course.value}${socket.user?.ref_id.branch.value}${socket.user?.ref_id.year.value}`);
+                    io.to(`${socket?.user?.ref_id?.branch?.value}/${socket?.user?.ref_id?.year?.value}`).emit(
+                        'newUserAreConnect', { onlineUsers: [...localdata] }
+                    );
+                } else {
+                    socket.emit('process-error-501', {
+                        message: "something was wrong!",
+                        submessage: "try to reload and sure about connectivity and to check login or not",
+                        success: false,
+                    });
 
-            await socket.join(`${socket?.user?.ref_id?.branch?.value}/${socket?.user?.ref_id?.year?.value}`)
-            io.to(`${socket?.user?.ref_id?.branch?.value}/${socket?.user?.ref_id?.year?.value}`).emit(
-                'newUserAreConnect', { onlineUsers: [...tempStorage] }
-            );
-
+                    return;
+                }
+            } else {
+                return;
+            }
 
             socket.on('socket_send_payload', async (data, callback) => {
                 const { senderId, receiverId, message, index, socketId, type, r_email, language } = data;
-                const findKey = [...tempStorage].reduce((acum, item) => {
-                    if (item[1].email.trim() == r_email.trim()) {
-                        acum = item[0];
-                    };
-
-                    return acum;
-                }, null)
-
-                if (findKey) {
+                const findKey = await socketSingleton.getSocketID(r_email.trim());
+                if (findKey.length > 0 && Array.isArray(findKey)) {
                     const findConnectionfirst = await connectionModel.findOne({ id: `${senderId}/${receiverId}` });
                     const findConnectionSecond = await connectionModel.findOne({ id: `${receiverId}/${senderId}` });
                     if (findConnectionfirst) {
@@ -82,7 +86,11 @@ const connectWithSocket = (server) => {
                         await savedataMessage.save();
                         findConnectionfirst.messages.push({ ref_id: savedataMessage._id });
                         await findConnectionfirst.save();
-                        io.to(findKey).emit("notification_new_message", { notify: "you receive new message", index, message: savedataMessage, distination: `${receiverId}/${senderId}`, chatID: findConnectionfirst?._id, senderId })
+
+                        for (let key of findKey) {
+                            io.to(key).emit("notification_new_message", { notify: "you receive new message", index, message: savedataMessage, distination: `${receiverId}/${senderId}`, chatID: findConnectionfirst?._id, senderId })
+                        }
+
                         callback({ notify: "successfully send your message", index, message: savedataMessage, distination: `${senderId}/${receiverId}` });
                     } else if (findConnectionSecond) {
                         const messagePayload = {
@@ -98,10 +106,14 @@ const connectWithSocket = (server) => {
                         const instanceMessage = await savedataMessage.save();
                         findConnectionSecond.messages.push({ ref_id: instanceMessage._id });
                         await findConnectionSecond.save();
-                        io.to(findKey).emit("notification_new_message", {
-                            notify: "you receive new message", index, message: savedataMessage,
-                            distination: `${receiverId}/${senderId}`, chatID: findConnectionSecond?._id, senderId
-                        })
+
+                        for (let key of findKey) {
+                            io.to(key).emit("notification_new_message", {
+                                notify: "you receive new message", index, message: savedataMessage,
+                                distination: `${receiverId}/${senderId}`, chatID: findConnectionSecond?._id, senderId
+                            })
+                        }
+
                         callback({ notify: "successfully send your message", index, message: savedataMessage, distination: `${senderId}/${receiverId}` });
                     } else {
                         const createNewConnection = new connectionModel({
@@ -122,12 +134,16 @@ const connectWithSocket = (server) => {
                         const instanceMessage = await savedataMessage.save();
                         createNewConnection.messages.push({ ref_id: instanceMessage._id });
                         await createNewConnection.save();
-                        io.to(findKey).emit("notification_new_message", {
-                            notify: "you receive new message (new connection)",
-                            index, message: savedataMessage,
-                            distination: `${senderId}/${receiverId}`,
-                            chatID: createNewConnection?._id, senderId
-                        })
+
+                        for (let key of findKey) {
+                            io.to(key).emit("notification_new_message", {
+                                notify: "you receive new message (new connection)",
+                                index, message: savedataMessage,
+                                distination: `${senderId}/${receiverId}`,
+                                chatID: createNewConnection?._id, senderId
+                            })
+                        }
+
                         callback({ notify: "successfully send your message first time", index, message: savedataMessage, distination: `${senderId}/${receiverId}`, senderId, receiverId });
                     }
                 } else {
@@ -136,7 +152,7 @@ const connectWithSocket = (server) => {
             })
 
             socket.on('socket_send_payload_offline_user', async (data, callback) => {
-                // callback - function to action on sender device, data = payload
+                // callback - function make action on sender device, data = payload
                 try {
                     const response = await acceptOfflineMessages(data);
                     callback(response);
@@ -153,17 +169,17 @@ const connectWithSocket = (server) => {
                 io.to(socketId).emit('receive_user_typing_off', { mode: "stop_typing", email });
             })
 
-            socket.on('disconnect', (reason) => {
-                console.log(`${socket.id} are disconnect ${reason}`);
-                tempStorage.delete(socket.id);
-
-                io.to(`${socket?.user?.ref_id?.branch?.value}/${socket?.user?.ref_id?.year?.value}`).emit(
-                    'userAreDisconnect', { onlineUsers: [...tempStorage] }
-                )
+            socket.on('disconnect', async (reason) => {
+                const permission_to_delete = await socketSingleton.deleteUser(socket?.user?.ref_id.email, socket.id, socket?.user)
+                if (permission_to_delete) {
+                    const localdata = await socketSingleton.getAllOnlineUser(`${socket.user?.ref_id.course.value}${socket.user?.ref_id.branch.value}${socket.user?.ref_id.year.value}`);
+                    io.to(`${socket?.user?.ref_id?.branch?.value}/${socket?.user?.ref_id?.year?.value}`).emit(
+                        'userAreDisconnect', { onlineUsers: [...localdata] }
+                    )
+                }
             })
         });
 
-        console.log("socket.io are connected with geman.ai");
         return io;
     } catch (error) {
         throw new Error(`Error-socket.io  ${error}`)
--- Frontend/src/ReduxStore/utils/ConvertMapToArray.js
+++ Frontend/src/ReduxStore/utils/ConvertMapToArray.js
@@ -1,10 +1,18 @@
 
 
-const convertMapToArray = (map) => {
-    const obj = Object.fromEntries(map);
+const convertMapToArray = (usersList) => {
+
+    // ================= old =============================
+    // const obj = Object.fromEntries(map);
     const temp_online_user = []
-    for (let key in obj) {
-        temp_online_user.push({ socketId: key, ...obj[key].ref_id, GSS: true })
+    // for (let key in obj) {
+    //     temp_online_user.push({ socketId: key, ...obj[key].ref_id, GSS: true })
+    // }
+
+
+    // ====================new version - 29/03/2026 - saurabh ===================
+    for (let value of usersList) {
+        temp_online_user.push({ ...value, GSS: true });
     }
 
     return temp_online_user
