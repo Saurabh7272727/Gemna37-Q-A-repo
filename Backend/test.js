95713d4d9bad76e6603d9a9b913f97fd83866572
saurabh sharma <saurabhsharma63679383@gmail.com>
2026-04-13 20:08:35 +0530

    we add logs file - because error rise in vercel logs not found

--- Backend/test.js
+++ Backend/test.js
@@ -9,13 +9,13 @@ function generatePayload() {
 }
 
 autocannon({
-    url: 'http://localhost:3000/student/login', // apna route yaha daal
-    method: 'POST',
-    connections: 1550,   // concurrent users
-    duration: 20,      // seconds
+    url: 'https://gemna37-q-a-repo.onrender.com/api/v1/students/connection', // apna route yaha daal
+    method: 'GET',
+    connections: 40,   // concurrent users
+    duration: 10,      // seconds
     pipelining: 1,
     headers: {
-        'Content-Type': 'application/json'
+        "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4ZWI4MzhjMzM2NzIwYjA3NzU3YzBjZCIsImNyZWRlbnRpYWxIYXNoIjoiJDJiJDEwJGlycktVbGhXYUxaNFBTSkZDSFV2TC56REp3cDU4OW93UzJsUDRldFVNUWt4bkxkQ1BRUG1tIiwiaWF0IjoxNzc2MDU4MjA1LCJleHAiOjE3NzYwNjU0MDV9.PY7DZ4XSzquWvx6bJjOnaf2SVMXoBzmaD3ctPhXks3k`
     },
 
     setupClient: (client) => {
